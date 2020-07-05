484bceb773c899f05f7bdf1fd4dc99fcf2856da4
FenixTrax18 <60018827+FenixTrax18@users.noreply.github.com>
2020-07-05 13:44:03 -0400

    added mongoose-extend-schema module and set up extended subclasses

--- models/model.js
+++ models/model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const extend = require("mongoose-schema-extend");
+
 
 const Schema = mongoose.Schema;
 
@@ -15,7 +17,7 @@ const ExerciseSchema = new Schema({
       message: type => `Exercise Type must be set to either 'resistance' or 'cardio'`
     }
   },
-  
+
   name: {
     type: String,
     trim: true,
@@ -26,9 +28,43 @@ const ExerciseSchema = new Schema({
     type: Number,
     required: "Exercise duration is required",
     min: [0, 'Must be greater than 0']
+  },
+
+},
+{
+  disciminatorKey: 'type'
+});
+
+const resistenceSchema = ExerciseSchema.extend({
+  weight:{
+    type: Number,
+    required: "Exercise weight is required",
+    min: [0, 'Must be greater than 0']
+  },
+  reps: {
+    type: Number,
+    required: "Exercise reps is required",
+    min: [0, 'Must be greater than 0']
+  },
+  sets: {
+    type: Number,
+    required: "Exercise sets is required",
+    min: [0, 'Must be greater than 0']
   }
 });
 
+const cardioSchema = ExerciseSchema.extend({
+  distance: {
+    type: Number,
+    required: "Exercise distance is required",
+    min: [0, 'Must be greater than 0']
+  }
+})
+
 const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+const Resistence = mongoose.model("Resistence", resistenceSchema);
+
+const Cardio = mongoose.model("Cardio", cardioSchema);
 
 module.exports = Exercise;
